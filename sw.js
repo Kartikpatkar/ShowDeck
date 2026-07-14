2e436b9ea61913b70f32c514b0af7efc8e93263c
Kartik Patkar <kartikpatkar18@gmail.com>
2026-07-15 00:11:01 +0530

    fix: resolve mobile navigation overlap and increment cache version to 1.6.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'showdeck-v6';
-const IMAGE_CACHE_NAME = 'showdeck-images-v6';
+const CACHE_NAME = 'showdeck-v7';
+const IMAGE_CACHE_NAME = 'showdeck-images-v7';
 
 const URLS_TO_CACHE = [
   './',
